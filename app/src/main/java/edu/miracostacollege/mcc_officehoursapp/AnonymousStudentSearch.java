05804c1e7bdb3c595eaae9812e5fac3df6e76ecd
Zsuzsanna Dianovics <zsudever@yahoo.com>
2020-04-29 10:54:47 -0700

    fixed manifest. added studentRegistration activity

---
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/AnonymousStudentSearch.java
@@ -0,0 +1,16 @@
+package edu.miracostacollege.mcc_officehoursapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnonymousStudentSearch extends AppCompatActivity {
+
+
+    //testig branch
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anonymous_student_search);
+    }
+}
