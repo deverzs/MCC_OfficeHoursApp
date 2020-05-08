c7c9b68337c1993ddbc1e2b6ce1030b16d7275f4
fuzzell <sfuzzell@gmail.com>
2020-05-07 19:25:13 -0700

    newest update for Sarah

---
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/updateScheduleActivity.java
@@ -0,0 +1,18 @@
+package edu.miracostacollege.mcc_officehoursapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class updateScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_schedule);
+
+
+    }
+
+
+}
