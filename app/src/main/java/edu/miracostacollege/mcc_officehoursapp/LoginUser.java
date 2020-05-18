ef5d243741ed115fbf6c074b0e8f17241fe67ef1
Zsuzsanna Dianovics <zsudever@yahoo.com>
2020-05-18 11:54:49 -0700

    updated csv, registration working

--- app/src/main/java/edu/miracostacollege/mcc_officehoursapp/LoginUser.java
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/LoginUser.java
@@ -2,10 +2,10 @@ package edu.miracostacollege.mcc_officehoursapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,16 +22,8 @@ public class LoginUser extends AppCompatActivity {
 
 
     public static final String TAG = LoginUser.class.getSimpleName();
-
-    //not sure if add here or only in activity that needs it? or on startup of app?
     private DBHelper db;
-    private List<Instructor> allInstructorList;
-    private List<Schedule> allScheduleList;
     private List<Login> allLoginList;
-    private  List<Verification> allVerificationList;
-
-
-
 
 
     @Override
@@ -39,57 +31,55 @@ public class LoginUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
 
-        deleteDatabase(DBHelper.DATABASE_NAME);
+        //deleteDatabase(DBHelper.DATABASE_NAME);
         db = new DBHelper(this);
         db.importInstructorsFromCSV("instructor.csv");
         db.importScheduleFromCSV("schedule.csv");
         db.importVerifiedFromCSV("verified.csv");
 
-        allInstructorList = db.getAllInstructors();
-        allScheduleList = db.getAllSchedules();
-        allVerificationList = db.getAllVerifications();
-        allLoginList = db.getAllLogin();
-
     }
 
-    public void handleButtons(View v)
+    public void handleLoginButton(View v)
     {
-        Button button = (Button) v;
-        Button searchOnlyButton = findViewById(R.id.searchOnlyButton_LOGIN);
-        Button loginButton = findViewById(R.id.loginButton_LOGIN);
+        allLoginList = db.getAllLogin();
         EditText emailEditText = findViewById(R.id.emailEditText_LOGIN);
         EditText passwordEditText = findViewById(R.id.passwordEditText_LOGIN);
+        boolean notValid = true;
 
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        if(button == loginButton) {
-            for (Login login : allLoginList) {
-                if (email.equals(login.getmEmail())) {
-                    if (password.equals(login.getmPassowrd())) {
-                        if (login.getIsProfessor() == 1) {
-                            //professor
-                            Intent intentProf = new Intent(this, ProfessorLoggedInView.class);
-                            startActivity(intentProf);
-                        } else {
-                            //student
-                            Intent intentStudent = new Intent(this, LoggedinSavedProfs.class);
-                            startActivity(intentStudent);
-                        }
+
+        for (Login login : allLoginList) {
+            if (email.equals(login.getmEmail())) {
+                if (password.equals(login.getmPassowrd())) {
+                    notValid = false;
+                    if (login.getIsProfessor() == 1) {
+                         //professor
+                         Intent intentProf = new Intent(this, ProfessorLoggedInView.class);
+                         startActivity(intentProf);
+                    } else {
+                         //student
+                         Intent intentStudent = new Intent(this, LoggedinSavedProfs.class);
+                         startActivity(intentStudent);
                     }
-
                 }
-
             }
-            Toast.makeText(this, "Password and email do not match", Toast.LENGTH_LONG).show();
-            emailEditText.setText("");
-            passwordEditText.setText("");
-        } else if(button == searchOnlyButton){
-            Intent searchIntent = new Intent(this, StudentSearch.class);
-            startActivity(searchIntent);
-
         }
 
+        if(notValid) {
+                Toast.makeText(this, "Password and email are not valid", Toast.LENGTH_LONG).show();
+                emailEditText.setText("");
+                passwordEditText.setText("");
+        }
+
+
+    }
+
+    public void handleSearchOnly(View v)
+    {
+        Intent searchIntent = new Intent(this, StudentSearch.class);
+        startActivity(searchIntent);
     }
 
     public void handleRegister(View v)
