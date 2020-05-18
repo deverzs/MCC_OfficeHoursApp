99b728dc45b956ac6e36ff2e45b9ec4e09d94291
fuzzell <sfuzzell@gmail.com>
2020-05-18 12:00:55 -0700

    Merge branch 'Zsuzsanna_2' of https://github.com/deverzs/MCC_OfficeHoursApp into Sarah
     Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/edu/miracostacollege/mcc_officehoursapp/ProfessorLoggedInView.java
    	app/src/main/java/edu/miracostacollege/mcc_officehoursapp/SplashActivity.java
    	app/src/main/java/edu/miracostacollege/mcc_officehoursapp/updateScheduleActivity.java
    	app/src/main/res/layout/activity__registration.xml
    	app/src/main/res/layout/activity_login.xml
    	app/src/main/res/layout/activity_professor_logged_in_view.xml
    	app/src/main/res/layout/activity_splash.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/edu/miracostacollege/mcc_officehoursapp/ViewScheduleActivity.java
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/ViewScheduleActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class ViewScheduleActivity extends AppCompatActivity {
 //
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
