e0301dc5537344d904b548553a3d69e9ed99fb2d
Zsuzsanna Dianovics <zsudever@yahoo.com>
2020-05-19 08:53:35 -0700

    fixed crash from schedule.csv import

--- app/src/main/java/edu/miracostacollege/mcc_officehoursapp/LoggedinSavedProfs.java
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/LoggedinSavedProfs.java
@@ -12,6 +12,7 @@ import java.util.List;
 import edu.miracostacollege.mcc_officehoursapp.Model.DBHelper;
 import edu.miracostacollege.mcc_officehoursapp.Model.Instructor;
 import edu.miracostacollege.mcc_officehoursapp.Model.SavedInstructor;
+import edu.miracostacollege.mcc_officehoursapp.Model.Schedule;
 import edu.miracostacollege.mcc_officehoursapp.Model.professorsListViewAdapter;
 
 import static edu.miracostacollege.mcc_officehoursapp.Model.DBHelper.DATABASE_NAME;
@@ -21,9 +22,11 @@ public class LoggedinSavedProfs extends AppCompatActivity {
 
     private static final String TAG = LoggedinSavedProfs.class.getSimpleName();
     private DBHelper db;
+
     private List<SavedInstructor> instructorList;
     private SavedInstructorListAdapter instructorListAdapter;
     private ListView instructorListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +34,20 @@ public class LoggedinSavedProfs extends AppCompatActivity {
         setContentView(R.layout.activity_loggedin_saved_profs);
         db = new DBHelper(this);
 
-        db.addSavedInstructor(2);
-        db.addSavedInstructor(3);
+       // deleteDatabase(DATABASE_NAME);
+        db.addSavedInstructor(2); //dummy
+        db.addSavedInstructor(3); //dummy
+
         instructorList = db.getAllSavedInstructors();
-        instructorListAdapter = new SavedInstructorListAdapter(this, R.layout.activity_professor_list_item, instructorList);
+
+        //Instructor List Adapter
+        instructorListAdapter = new SavedInstructorListAdapter(this,
+                R.layout.activity_professor_list_item, instructorList);
 
         instructorListView = findViewById(R.id.professorsListView_SAVED);
         instructorListView.setAdapter(instructorListAdapter);
+
+
 
     }
 
@@ -45,7 +55,14 @@ public class LoggedinSavedProfs extends AppCompatActivity {
         Instructor selectedInstuctor = (Instructor) v.getTag();
 
         Intent intent = new Intent(this, ProfessorDetails.class);
+        intent.putExtra("Instructor", selectedInstuctor.getmId());
+        startActivity(intent);
 
+    }
 
+    public void handleNewSearch(View v)
+    {
+        Intent intent = new Intent(this, StudentSearch.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/miracostacollege/mcc_officehoursapp/ProfessorDetails.java
+++ app/src/main/java/edu/miracostacollege/mcc_officehoursapp/ProfessorDetails.java
@@ -2,13 +2,55 @@ package edu.miracostacollege.mcc_officehoursapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import edu.miracostacollege.mcc_officehoursapp.Model.DBHelper;
+import edu.miracostacollege.mcc_officehoursapp.Model.Instructor;
+import edu.miracostacollege.mcc_officehoursapp.Model.Schedule;
 
 public class ProfessorDetails extends AppCompatActivity {
+
+    private List<Schedule> scheduleList ;
+    private List<Schedule> scheduleListSelected ;
+    private ScheduleListAdapter scheduleListAdapter;
+    private ListView scheduleListView;
+    private DBHelper db;
+
+    TextView professorNameTextView;
+    TextView professorPhoneTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor_details);
+        professorNameTextView  = findViewById(R.id.professorNameTextView_DETAILS);
+        professorPhoneTextView = findViewById(R.id.extensionTextView_DETAILS);
+        db = new DBHelper(this);
+        scheduleList = db.getAllSchedules();
+
+        Intent intent = getIntent();
+        Long instructorID = intent.getLongExtra("Instructor", 0);
+        Instructor instructor = db.getInstructor(instructorID);
+
+        professorNameTextView.setText(instructor.getmFullName());
+        professorPhoneTextView.setText(instructor.getmPhone());
+
+        for(Schedule s: scheduleList){
+            if(s.getmInstructor().getmId()==instructorID){
+                scheduleListSelected.add(s);
+            }
+        }
+        //Schedule List Adapter
+        scheduleListAdapter = new ScheduleListAdapter(this,
+                R.layout.activity_schedule_list_item, scheduleListSelected);
+        scheduleListView = findViewById(R.id.sessionListView_DETAILS);
+        scheduleListView.setAdapter(scheduleListAdapter);
     }
 }
